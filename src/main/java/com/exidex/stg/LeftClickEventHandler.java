e04c811f5fda6ddba12e40895f18e3b9b01ef6dd
Exidex <mrexidex@gmail.com>
2017-12-09 15:20:40 +0100

    Filter out FakePlayers from list of entities that could be damaged.
    Default to destroying grass-like blocks instead of going 'through' them and add config option for it.
    Allow vanilla client to connect to server with this mod installed.
    Refactor some stuff.

--- src/main/java/com/exidex/stg/LeftClickEventHandler.java
+++ src/main/java/com/exidex/stg/LeftClickEventHandler.java
@@ -1,73 +1,100 @@
 package com.exidex.stg;
 
 import com.google.common.base.Predicates;
+import com.google.common.collect.Lists;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.EntitySelectors;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+import net.minecraftforge.common.util.FakePlayer;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.util.List;
+import java.util.function.Function;
 
 /**
  * @author exidex.
  * @since 02.04.2017.
  */
+@Mod.EventBusSubscriber
 public final class LeftClickEventHandler {
+
+	public static final List<Function<EntityLivingBase, Boolean>> PREDICATES = Lists.newArrayList();
 
 	private LeftClickEventHandler() {}
 
 	@SubscribeEvent
 	public static void onLeftClick(PlayerInteractEvent.LeftClickBlock event) {
 
-		IBlockState state = event.getWorld().getBlockState(event.getPos()).getActualState(event.getWorld(),event.getPos());
-		if (state.getCollisionBoundingBox(event.getWorld(), event.getPos()) != Block.NULL_AABB)
+		IBlockState state = event.getWorld().getBlockState(event.getPos()).getActualState(event.getWorld(), event.getPos());
+		if (state.getCollisionBoundingBox(event.getWorld(), event.getPos()) != Block.NULL_AABB) {
 			return;
+		}
 
-		EntityPlayer entityPlayer = event.getEntityPlayer();
+		EntityPlayer player = event.getEntityPlayer();
+		if (player == null) {
+			return;
+		}
 
 		float blockReachDistance = 4.5F;
 
-		Vec3d from = new Vec3d(entityPlayer.posX, entityPlayer.posY + (double)entityPlayer.getEyeHeight(), entityPlayer.posZ);
-		Vec3d vec3d = entityPlayer.getLook(1.0F);
+		Vec3d from = new Vec3d(player.posX, player.posY + (double) player.getEyeHeight(), player.posZ);
+		Vec3d vec3d = player.getLook(1.0F);
 		Vec3d to = from.addVector(vec3d.x * blockReachDistance, vec3d.y * blockReachDistance, vec3d.z * blockReachDistance);
 
-		Entity targetEntity = getEntityClosestToStartPos(entityPlayer, event.getWorld(), from, to);
+		EntityLivingBase targetEntity = getEntityClosestToStartPos(player, event.getWorld(), from, to);
 
-		if(targetEntity != null) {
+		if (targetEntity != null) {
 			if (!event.getWorld().isRemote) {
-				entityPlayer.attackTargetEntityWithCurrentItem(targetEntity);
-				entityPlayer.resetCooldown();
+				player.attackTargetEntityWithCurrentItem(targetEntity);
+				player.resetCooldown();
 			}
-			event.setCanceled(true);
+			event.setCanceled(STGConfig.cancelClickEventPropagation);
 		}
 	}
 
-	private static Entity getEntityClosestToStartPos(Entity entityIn, World world, Vec3d startPos, Vec3d endPos) {
-		Entity entity = null;
-		List<Entity> list = world.getEntitiesInAABBexcluding(entityIn, new AxisAlignedBB(startPos.x,startPos.y,startPos.z, endPos.x, endPos.y, endPos.z), Predicates.and(EntitySelectors.NOT_SPECTATING, entity1 -> entity1 != null && entity1.canBeCollidedWith()));
+	private static EntityLivingBase getEntityClosestToStartPos(EntityPlayer player, World world, Vec3d startPos, Vec3d endPos) {
+		EntityLivingBase entityLiving = null;
+		List<Entity> list = world.getEntitiesInAABBexcluding(player,
+				new AxisAlignedBB(startPos.x, startPos.y, startPos.z, endPos.x, endPos.y, endPos.z),
+				Predicates.and(EntitySelectors.CAN_AI_TARGET,
+						e -> {
+							boolean filter = e != null && e.canBeCollidedWith()
+									&& e instanceof EntityLivingBase
+									&& !(e instanceof FakePlayer);
+
+							for (Function<EntityLivingBase, Boolean> predicate : PREDICATES) {
+								filter &= predicate.apply((EntityLivingBase) e);
+							}
+
+							return filter;
+						}
+				));
+
 		double d0 = 0.0D;
 		AxisAlignedBB axisAlignedBB;
 
-		for(Entity entity1 : list) {
-			axisAlignedBB = entity1.getEntityBoundingBox().expand(0.3D, 0.3D, 0.3D);
+		for (Entity entity : list) {
+			axisAlignedBB = entity.getEntityBoundingBox().expand(0.3D, 0.3D, 0.3D);
 			RayTraceResult raytraceResult = axisAlignedBB.calculateIntercept(startPos, endPos);
 
-			if(raytraceResult != null) {
+			if (raytraceResult != null) {
 				double d1 = startPos.squareDistanceTo(raytraceResult.hitVec);
 
-				if(d1 < d0 || d0 == 0.0D) {
-					entity = entity1;
+				if (d1 < d0 || d0 == 0.0D) {
+					entityLiving = (EntityLivingBase) entity;
 					d0 = d1;
 				}
 			}
 		}
-		return entity;
+		return entityLiving;
 	}
 }
--- src/main/java/com/exidex/stg/STG.java
+++ src/main/java/com/exidex/stg/STG.java
@@ -1,26 +1,31 @@
 package com.exidex.stg;
 
-import net.minecraftforge.common.MinecraftForge;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInterModComms;
 
 /**
  * @author exidex.
  * @since 02.04.2017.
  */
-@Mod(modid = STG.MODID, name = STG.NAME, version = STG.VERSION, acceptedMinecraftVersions = "[1.12,1.13)", acceptableRemoteVersions = "*")
+@Mod(modid = STG.MODID,
+		name = STG.NAME,
+		version = STG.VERSION,
+		acceptedMinecraftVersions = "[1.12,1.13)",
+		acceptableRemoteVersions = "*")
 public class STG {
 
     public static final String MODID = "stg";
-    public static final String VERSION = "1.12-1.2.1";
+    public static final String VERSION = "@VERSION@";
     public static final String NAME = "SwingThroughGrass";
 
     @Mod.Instance(STG.MODID)
 	public static STG instance;
-    
-    @EventHandler
-    public void init(FMLInitializationEvent event) {
-		MinecraftForge.EVENT_BUS.register(LeftClickEventHandler.class);
-    }
+
+    @Mod.EventHandler
+    public void handleIMCMassages(FMLInterModComms.IMCEvent event) {
+    	event.getMessages().forEach(imcMessage -> {
+			imcMessage.getFunctionValue(EntityLivingBase.class, Boolean.class).ifPresent(LeftClickEventHandler.PREDICATES::add);
+		});
+	}
 }
