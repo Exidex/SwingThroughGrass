e3e46638b96a6fbc6c87be9c56144bfff901c40f
GameWorld <ehrenf@outlook.com>
2024-01-03 18:19:12 +1000

    port to 1.7.10

--- src/main/java/com/exidex/stg/LeftClickEventHandler.java
+++ src/main/java/com/exidex/stg/LeftClickEventHandler.java
@@ -1,79 +1,38 @@
 package com.exidex.stg;
 
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.eventhandler.EventPriority;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.AxisAlignedBB;
-import net.minecraft.util.DamageSource;
-import net.minecraft.util.MovingObjectPosition;
-import net.minecraft.util.Vec3;
+import net.minecraft.util.ChatComponentText;
 import net.minecraft.world.World;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
-import net.minecraftforge.event.entity.player.PlayerInteractEvent.Action;
 
 import java.util.List;
 
-@Mod(modid = STG.MODID, name = STG.NAME, version = STG.VERSION)
 public class LeftClickEventHandler {
+    @SubscribeEvent
+    public void onLeftClick(PlayerInteractEvent event) {
+        if (event.action == PlayerInteractEvent.Action.LEFT_CLICK_BLOCK) {
+            World world = event.entityPlayer.worldObj;
+            int x = event.x;
+            int y = event.y;
+            int z = event.z;
 
-	@Mod.Instance(STG.MODID)
-	public static LeftClickEventHandler instance;
+            // Check if the block is walkable
+            if (world.getBlock(x, y, z).getCollisionBoundingBoxFromPool(world, x, y, z) == null) {
+                System.out.println("Player left-clicked at a walkable block: " + x + ", " + y + ", " + z);
 
-	private LeftClickEventHandler() {
-		MinecraftForge.EVENT_BUS.register(this);
-	}
+                // Check for entities in the clicked block
+                AxisAlignedBB blockBoundingBox = AxisAlignedBB.getBoundingBox(x, y, z, x + 1, y + 1, z + 1);
+                List<Entity> entitiesInBlock = world.getEntitiesWithinAABB(Entity.class, blockBoundingBox);
 
-	@SubscribeEvent(priority = EventPriority.NORMAL)
-	public void onLeftClick(PlayerInteractEvent event) {
-		if (event.action == Action.LEFT_CLICK_BLOCK) {
-			Block block = event.world.getBlock(event.x, event.y, event.z);
-			if (block != null && block.getCollisionBoundingBoxFromPool(event.world, event.x, event.y, event.z) != null)
-				return;
-
-			EntityPlayer entityPlayer = event.entityPlayer;
-
-			float blockReachDistance = 4.5F;
-
-			Vec3 from = Vec3.createVectorHelper(entityPlayer.posX, entityPlayer.posY + (double) entityPlayer.getEyeHeight(), entityPlayer.posZ);
-			Vec3 vec3d = entityPlayer.getLook(1.0F);
-			Vec3 to = from.addVector(vec3d.xCoord * blockReachDistance, vec3d.yCoord * blockReachDistance, vec3d.zCoord * blockReachDistance);
-
-			Entity targetEntity = getEntityClosestToStartPos(entityPlayer, event.world, from, to);
-
-			if (targetEntity != null) {
-				if (!event.world.isRemote) {
-					// Assuming a 0-damage attack to mimic a left click
-					entityPlayer.attackEntityFrom(DamageSource.generic, 0);
-				}
-				event.setCanceled(true);
-			}
-		}
-	}
-
-	private Entity getEntityClosestToStartPos(Entity entityIn, World world, Vec3 startPos, Vec3 endPos) {
-		Entity entity = null;
-		double d0 = 0.0D;
-		List<Entity> list = world.getEntitiesWithinAABBExcludingEntity(entityIn, entityIn.boundingBox.addCoord(endPos.xCoord - startPos.xCoord, endPos.yCoord - startPos.yCoord, endPos.zCoord - startPos.zCoord).expand(1.0, 1.0, 1.0));
-		for (Entity entity1 : list) {
-			if (entity1.canBeCollidedWith()) {
-				float f = 0.3F;
-				AxisAlignedBB axisalignedbb = entity1.boundingBox.expand((double) f, (double) f, (double) f);
-				MovingObjectPosition movingobjectposition = axisalignedbb.calculateIntercept(startPos, endPos);
-
-				if (movingobjectposition != null) {
-					double d1 = startPos.squareDistanceTo(movingobjectposition.hitVec);
-
-					if (d1 < d0 || d0 == 0.0D) {
-						entity = entity1;
-						d0 = d1;
-					}
-				}
-			}
-		}
-		return entity;
-	}
+                if (!entitiesInBlock.isEmpty()) {
+                    // There is at least one entity in the clicked block
+                    // Hit the first entity in the list
+                    Entity targetEntity = entitiesInBlock.get(0);
+                    event.entityPlayer.attackTargetEntityWithCurrentItem(targetEntity);
+                }
+            }
+        }
+    }
 }
--- src/main/java/com/exidex/stg/STG.java
+++ src/main/java/com/exidex/stg/STG.java
@@ -1,15 +1,23 @@
 package com.exidex.stg;
 
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import net.minecraft.init.Blocks;
+import net.minecraftforge.common.MinecraftForge;
 
 @Mod(modid = STG.MODID, name = STG.NAME, version = STG.VERSION, acceptableRemoteVersions = "*")
 public class STG {
-
     public static final String MODID = "stg";
     public static final String VERSION = "@VERSION@";
     public static final String NAME = "SwingThroughGrass";
 
-    @Instance(STG.MODID)
-        public static STG instance;
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+        // Register the event handler
+        MinecraftForge.EVENT_BUS.register(new LeftClickEventHandler());
+
+        // Some example code
+        System.out.println("DIRT BLOCK >> " + Blocks.dirt.getUnlocalizedName());
+    }
 }
